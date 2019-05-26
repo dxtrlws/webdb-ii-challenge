ff07be0525b4afd7be01cada2903c75cde22875c
Dexter Lewis <dexter.lewis@gmail.com>
2019-05-26 17:36:11 -0400

    add get and post for zoos

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex')
+
+const config = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3'
+  },
+  useNullAsDefault: true,
+}
+
+const db = knex(config)
 
 const server = express();
 
@@ -7,6 +18,26 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+  .then(data => {
+    res.status(200).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+server.post('/api/zoos', (req, res) => {
+  db('zoos').insert(req.body)
+  .then(data => {
+    res.status(201).json(data)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
 
 const port = 3300;
 server.listen(port, function() {
